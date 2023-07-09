200331ec03e38c21cdbe12bc69f90b805c7752a0
chistyakovdennis <chistyakov.d@gmail.com>
2023-07-09 15:23:11 +0300

    варианты решения задач

--- Task 68/Program.cs
+++ Task 68/Program.cs
@@ -3,3 +3,34 @@
 // // m = 3, n = 2 -> A(m,n) = 29
 // Функция Аккермана — простой пример вычислимой функции, которая не является примитивно рекурсивной. 
 // Она обозначается A(m,n), принимает два неотрицательных целых числа в качестве параметров и возвращает натуральное число.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m, n);
+
+// Вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+
+// Функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
